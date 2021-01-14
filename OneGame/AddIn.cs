34b0872bc630b48ac46d80dfcfb3738137172966
nichthai <nichthai2004@gmail.com>
2021-01-14 13:25:43 -0600

    c

--- OneGame/AddIn.cs
+++ OneGame/AddIn.cs
@@ -8,16 +8,12 @@ using System.Windows.Forms;
 
 namespace OneGame
 {
-    [GuidAttribute("CD37B0D2-B19E-4B9A-BD66-12573E8DBA04"), ProgId("OneGame.Class1")]
-    public class Class1 : IDTExtensibility2, IRibbonExtensibility
+    [Guid("CD37B0D2-B19E-4B9A-BD66-12573E8DBA04"), ProgId("OneGame.AddIn")]
+    public class AddIn : IDTExtensibility2, IRibbonExtensibility
     {
         Application onApp = new Application();
 
-        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
-        {
-            onApp = (Application)Application;
-        }
-
+        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom) => onApp = (Application)Application;
         public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
         {
             onApp = null;
@@ -26,20 +22,15 @@ namespace OneGame
         }
         public void OnAddInsUpdate(ref Array custom) { }
         public void OnStartupComplete(ref Array custom) { }
-        public void OnBeginShutdown(ref Array custom)
-        {
-            if (onApp != null) onApp = null;
-        }
+        public void OnBeginShutdown(ref Array custom) => onApp = null;
+        public string GetCustomUI(string RibbonID) => Properties.Resources.ribbon;
 
-        public string GetCustomUI(string RibbonID)
-        {
-            return Properties.Resources.ribbon;
-        }
-
-        public void showHello(IRibbonControl control)
+        public void ShowHello(IRibbonControl control)
         {
             string id = onApp.Windows.CurrentWindow.CurrentPageId;
             MessageBox.Show("Current Page ID = " + id, "Hello World!");
+            //new Form1().Show();
+            //CreateChildForm();
         }
     }
 }
